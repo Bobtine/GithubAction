9e47ddc6a3dd297faba1ae5df553af7503a28f33
Tine <bobtine@live.fr>
2025-07-25 19:13:26 -0400

    Ajout appService

--- appservice.tf
+++ appservice.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "app_service_plan" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Windows"
-  sku_name            = "P1v2"
+  sku_name            = "F1"
 }
 
 resource "azurerm_windows_web_app" "app" {
