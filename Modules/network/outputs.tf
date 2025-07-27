dbaa3f4fd9c326362cadff639a7e7b3e5a18913b
Tine <bobtine@live.fr>
2025-07-26 21:35:18 -0400

    Structuration des folders app service

--- Modules/network/outputs.tf
+++ Modules/network/outputs.tf
@@ -9,3 +9,7 @@ output "sql_subnet_id" {
 output "bastion_subnet_id" {
   value = azurerm_subnet.bastion_subnet.id
 }
+
+output "vnet_name" {
+  value = azurerm_virtual_network.Poc_vnet.name
+}
--- Modules/sqlserver/outputs.tf
+++ Modules/sqlserver/outputs.tf
@@ -1,4 +1,12 @@
-## modules/sqlserver/outputs.tf
-output "sql_pe_private_ip" {
-  value = azurerm_private_endpoint.sql_pe.private_service_connection[0].private_ip_address
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.sql_server.fully_qualified_domain_name
 }
+
+output "sql_database_name" {
+  value = azurerm_mssql_database.sql_db.name
+}
+
+# Supprime cette sortie si tu n'as pas encore créé la ressource sql_pe :
+# output "sql_pe_private_ip" {
+#   value = azurerm_private_endpoint.sql_pe.private_service_connection[0].private_ip_address
+# }
